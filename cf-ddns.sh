a3b3a3509b5ce9a385108bd7a3b25e5acdcc716a
宇宙小哥 <52238373+yuzhouxiaogegit@users.noreply.github.com>
2022-10-25 22:53:56 +0800

    Update cf-ddns.sh

--- cf-ddns.sh
+++ cf-ddns.sh
@@ -1,77 +1,124 @@
-#!/bin/bash
+#!/usr/bin/env bash
+set -o errexit
+set -o nounset
+set -o pipefail
+
+#cloudflare的密钥
+CFKEY=
 
 #cloudflare的帐号
-auth_email=""
-#cloudflare的密钥
-auth_key=""
+CFUSER=
+
 #cloudflare的顶级域名
-zone_name=""
+CFZONE_NAME=
+
 #cloudflare的对应ddns域名
-record_name=""
-#需要通知的邮箱
-email=""
+CFRECORD_NAME=
 
-#检查ip地址的网站
-ip=$(curl -s http://ipv4.icanhazip.com)
-#本地存储ip的文件
-ip_file="/usr/local/bin/ip.txt"
-#cloudflare的ids文件
-id_file="/usr/local/bin/cloudflare.ids"
-#cloudflare的日志文件
-log_file="/usr/local/bin/cloudflare.log"
+# 记录类型，A(IPv4)|AAAA(IPv6)，默认 IPv4
+CFRECORD_TYPE=A
 
-# 日志打印
-log() {
-    if [ "$1" ]; then
-        echo -e "[$(date)] - $1" >> $log_file
-    fi
-}
+# Cloudflare TTL 用于记录，在 120 到 86400 秒之间
+CFTTL=120
 
-#判断获取到ip是否正常
-if [ ! -n "$ip" ]; then
-	echo "IS NULL"
-	exit 0
+# 忽略本地文件，无论如何更新ip
+FORCE=false
+
+WANIPSITE="http://ipv4.icanhazip.com"
+
+# 检索 WAN ip 的站点
+if [ "$CFRECORD_TYPE" = "A" ]; then
+  :
+elif [ "$CFRECORD_TYPE" = "AAAA" ]; then
+  WANIPSITE="http://ipv6.icanhazip.com"
 else
-  	echo "NOT NULL"
+  echo "$CFRECORD_TYPE specified is invalid, CFRECORD_TYPE can only be A(for IPv4)|AAAA(for IPv6)"
+  exit 2
 fi
 
-#判断ip是否改变
-if [ -f $ip_file ]; then
-    old_ip=$(cat $ip_file)
-    if [ $ip == $old_ip ]; then
-        echo "IP has not changed."
-        exit 0
-    fi
+# 获取参数
+while getopts k:u:h:z:t:f: opts; do
+  case ${opts} in
+    k) CFKEY=${OPTARG} ;;
+    u) CFUSER=${OPTARG} ;;
+    h) CFRECORD_NAME=${OPTARG} ;;
+    z) CFZONE_NAME=${OPTARG} ;;
+    t) CFRECORD_TYPE=${OPTARG} ;;
+    f) FORCE=${OPTARG} ;;
+  esac
+done
+
+# 如果缺少所需的设置，请退出
+if [ "$CFKEY" = "" ]; then
+  echo "Missing api-key, get at: https://www.cloudflare.com/a/account/my-account"
+  echo "and save in ${0} or using the -k flag"
+  exit 2
+fi
+if [ "$CFUSER" = "" ]; then
+  echo "Missing username, probably your email-address"
+  echo "and save in ${0} or using the -u flag"
+  exit 2
+fi
+if [ "$CFRECORD_NAME" = "" ]; then 
+  echo "Missing hostname, what host do you want to update?"
+  echo "save in ${0} or using the -h flag"
+  exit 2
 fi
 
-#ip改变了
-#提交给cloudflare同步ip
-if [ -f $id_file ] && [ $(wc -l $id_file | cut -d " " -f 1) == 2 ]; then
-    zone_identifier=$(head -1 $id_file)
-    record_identifier=$(tail -1 $id_file)
-else
-    zone_identifier=$(curl -s -X GET "https://api.cloudflare.com/client/v4/zones?name=$zone_name" -H "X-Auth-Email: $auth_email" -H "X-Auth-Key: $auth_key" -H "Content-Type: application/json" | grep -Po '(?<="id":")[^"]*' | head -1 )
-    record_identifier=$(curl -s -X GET "https://api.cloudflare.com/client/v4/zones/$zone_identifier/dns_records?name=$record_name" -H "X-Auth-Email: $auth_email" -H "X-Auth-Key: $auth_key" -H "Content-Type: application/json"  | grep -Po '(?<="id":")[^"]*')
-    echo "$zone_identifier" > $id_file
-    echo "$record_identifier" >> $id_file
+# 如果主机名不是 FQDN
+if [ "$CFRECORD_NAME" != "$CFZONE_NAME" ] && ! [ -z "${CFRECORD_NAME##*$CFZONE_NAME}" ]; then
+  CFRECORD_NAME="$CFRECORD_NAME.$CFZONE_NAME"
+  echo " => Hostname is not a FQDN, assuming $CFRECORD_NAME"
 fi
 
-update=$(curl -s -X PUT "https://api.cloudflare.com/client/v4/zones/$zone_identifier/dns_records/$record_identifier" -H "X-Auth-Email: $auth_email" -H "X-Auth-Key: $auth_key" -H "Content-Type: application/json" --data "{\"id\":\"$zone_identifier\",\"type\":\"A\",\"name\":\"$record_name\",\"content\":\"$ip\"}")
-
-#更新失败
-if [[ $update == *"\"success\":false"* ]]; then
-    message="API UPDATE FAILED. DUMPING RESULTS:\n$update"
-    echo -e "$message"
-#邮件提醒更新失败原因
-	curl "http://blog.mojxtang.com/mail/sendmail.php?toemail=$email&title=FAIL&content=$record_name error:$message"
-    exit 0
+# 获取当前和旧的 WAN ip
+WAN_IP=`curl -s ${WANIPSITE}`
+WAN_IP_FILE=$HOME/.cf-wan_ip_$CFRECORD_NAME.txt
+if [ -f $WAN_IP_FILE ]; then
+  OLD_WAN_IP=`cat $WAN_IP_FILE`
 else
-#更新成功
-    message="IP changed to: $ip"
-    echo "$ip" > $ip_file
-    log "$message"
-    echo "$message"
-#邮件提醒，ip更换成功了。
-	curl "http://blog.mojxtang.com/mail/sendmail.php?toemail=$email&title=SUCCESS&content=$record_name IP Changed to: $ip"
+  echo "No file, need IP"
+  OLD_WAN_IP=""
+fi
 
+# 如果 WAN IP 未更改非 -f 标志，请在此处退出
+if [ "$WAN_IP" = "$OLD_WAN_IP" ] && [ "$FORCE" = false ]; then
+  echo "WAN IP Unchanged, to update anyway use flag -f true"
+  exit 0
+fi
+
+# 获取 zone_identifier & record_identifier
+ID_FILE=$HOME/.cf-id_$CFRECORD_NAME.txt
+if [ -f $ID_FILE ] && [ $(wc -l $ID_FILE | cut -d " " -f 1) == 4 ] \
+  && [ "$(sed -n '3,1p' "$ID_FILE")" == "$CFZONE_NAME" ] \
+  && [ "$(sed -n '4,1p' "$ID_FILE")" == "$CFRECORD_NAME" ]; then
+    CFZONE_ID=$(sed -n '1,1p' "$ID_FILE")
+    CFRECORD_ID=$(sed -n '2,1p' "$ID_FILE")
+else
+    echo "Updating zone_identifier & record_identifier"
+    CFZONE_ID=$(curl -s -X GET "https://api.cloudflare.com/client/v4/zones?name=$CFZONE_NAME" -H "X-Auth-Email: $CFUSER" -H "X-Auth-Key: $CFKEY" -H "Content-Type: application/json" | grep -Po '(?<="id":")[^"]*' | head -1 )
+    CFRECORD_ID=$(curl -s -X GET "https://api.cloudflare.com/client/v4/zones/$CFZONE_ID/dns_records?name=$CFRECORD_NAME" -H "X-Auth-Email: $CFUSER" -H "X-Auth-Key: $CFKEY" -H "Content-Type: application/json"  | grep -Po '(?<="id":")[^"]*' | head -1 )
+    echo "$CFZONE_ID" > $ID_FILE
+    echo "$CFRECORD_ID" >> $ID_FILE
+    echo "$CFZONE_NAME" >> $ID_FILE
+    echo "$CFRECORD_NAME" >> $ID_FILE
+fi
+
+# 如果更改 WAN，请更新 cloudflare
+echo "Updating DNS to $WAN_IP"
+
+RESPONSE=$(curl -s -X PUT "https://api.cloudflare.com/client/v4/zones/$CFZONE_ID/dns_records/$CFRECORD_ID" \
+  -H "X-Auth-Email: $CFUSER" \
+  -H "X-Auth-Key: $CFKEY" \
+  -H "Content-Type: application/json" \
+  --data "{\"id\":\"$CFZONE_ID\",\"type\":\"$CFRECORD_TYPE\",\"name\":\"$CFRECORD_NAME\",\"content\":\"$WAN_IP\", \"ttl\":$CFTTL}")
+
+if [ "$RESPONSE" != "${RESPONSE%success*}" ] && [ "$(echo $RESPONSE | grep "\"success\":true")" != "" ]; then
+  echo "Updated succesfuly!"
+  echo $WAN_IP > $WAN_IP_FILE
+  exit
+else
+  echo 'Something went wrong :('
+  echo "Response: $RESPONSE"
+  exit 1
 fi
